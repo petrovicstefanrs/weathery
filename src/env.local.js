d861925abb91b3682b8f7b7704b3bfef62f7d7a0
Stefan Petrovic <petrovic.stefan.rs@gmail.com>
2018-12-13 23:47:26 +0100

    gitignore

--- src/env.local.js
+++
@@ -1,8 +0,0 @@
-export default {
-	weather_api_endpoint: 'http://api.apixu.com/v1',
-	weather_api_key: '5261bfab89c44dedb6a160940181012',
-
-	unsplash_api_endpoint: 'https://api.unsplash.com/',
-	unsplash_api_key: '6ef57b06948470c14588a97c4b4abfff86fb1bc5f8cbe92de5db47e00023c182',
-	unsplash_api_version: 'v1',
-};
